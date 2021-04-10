5ce7eea4a3a11e7c9e21b98fc3f3a466598170d0
unknown <tom.westermann@hsu-hh.de>
2021-04-10 17:26:06 +0200

    added first example for UnitFilter

--- src/filters/UnitFilter.py
+++ src/filters/UnitFilter.py
@@ -8,18 +8,22 @@ class UnitFilter(Filter):
 	def applyFilter(self, dataFrame:pd.DataFrame):
 		print("UnitValueFilter. DataFrame: " + str(dataFrame))
 
-		pressure_values = np.array(dataFrame.loc[:,'PT102/OUT.CV'])
+		pressure_values = dataFrame.loc[:,'PT102/OUT.CV']
 
-		pressure_values = pressure_values.reshape(-1, 1)
+		pressure_values = pd.to_numeric(pressure_values)
 
-		clf = OneClassSVM(gamma='auto').fit(pressure_values)
-		
-		pressure_outlier = clf.predict(pressure_values)
+		new_pressure_values = []
+		for k in range(len(pressure_values)):
+			if pressure_values[k] < 0.3:
+				new_pressure_values.append(pressure_values[k] * 10)
+			else:
+				new_pressure_values.append(pressure_values[k])
+		dataFrame['PT102/OUT.CV'] = new_pressure_values
 
-		d = np.where(pressure_outlier>0)
-		print(d[0])
+		# d = np.where(pressure_outlier>0)
+		# print(d[0])
 
-		print(pressure_outlier)
+		# print(pressure_outlier)
 		# dataFrame = dataFrame.drop(['TIMESTAMP'], axis=1)
 		# Do something with the dataFrame
 		return dataFrame
