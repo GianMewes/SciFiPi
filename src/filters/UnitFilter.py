ea368723d8642deaaa85e548662efb8a5e5cc3e5
Westermann <tom.westermann@hsu-hh.de>
2021-05-04 16:53:39 +0200

    extended UnitFilter for all columns

--- src/filters/UnitFilter.py
+++ src/filters/UnitFilter.py
@@ -8,28 +8,35 @@ class UnitFilter(Filter):
 	def applyFilter(self, dataFrame:pd.DataFrame):
 		# print("UnitValueFilter. DataFrame: " + str(dataFrame))
 
-		pressure_values = dataFrame.loc[:,'PT102/OUT.CV']
+		for col in dataFrame.columns:
+			values = dataFrame.loc[:,col]
 
-		pressure_values = pd.to_numeric(pressure_values)
+			try:
+				values = pd.to_numeric(values)
 
-		# calculate change factor between two timestamps
-		change_factor = []
-		change_factor.append(0)
-		for k in range(1, len(pressure_values)):
-			change_factor.append(pressure_values[k]/pressure_values[k-1])
+				# calculate change factor between two timestamps
+				change_factor = []
+				change_factor.append(0)
+				for k in range(1, len(values)):
+					change_factor.append(values[k]/values[k-1])
 
-		multiplier = [round(x, 2) if x < 1 else round(x, 1) for x in change_factor]
-		multiplier_low = [i for i,x in enumerate(multiplier) if x==0.1]
-		multiplier_high = [i for i,x in enumerate(multiplier) if x==10]
+				multiplier = [round(x, 2) if x < 1 else round(x, 1) for x in change_factor]
+				multiplier_low = [i for i,x in enumerate(multiplier) if x==0.1]
+				multiplier_high = [i for i,x in enumerate(multiplier) if x==10]
 
-		for i in range(min(len(multiplier_high), len(multiplier_high))):
-			if multiplier_low[i] < multiplier_high[i]:
-				for n in range(multiplier_low[i], multiplier_high[i]):
-					pressure_values[n] = pressure_values[n] * 10
-			elif multiplier_low[i] > multiplier_high[i]:
-				for n in range(multiplier_high[i], multiplier_low[i]):
-					pressure_values[n] = pressure_values[n] / 10
+				for i in range(min(len(multiplier_high), len(multiplier_high))):
+					if multiplier_low[i] < multiplier_high[i]:
+						for n in range(multiplier_low[i], multiplier_high[i]):
+							values[n] = values[n] * 10
+						print('Detected comma error in ' + col + '. Multiplied values from row ' + str(multiplier_low) + ' to row ' + str(multiplier_high) + ' in ' + col + ' by 10.')
+					elif multiplier_low[i] > multiplier_high[i]:
+						for n in range(multiplier_high[i], multiplier_low[i]):
+							values[n] = values[n] / 10
+						print('Detected comma error in ' + col + '. Divided values from  row ' + str(multiplier_low) + ' to row ' + str(multiplier_high) + ' in ' + col + ' by 10.')
 
-		dataFrame['PT102/OUT.CV'] = pressure_values
+				dataFrame[col] = values
+				
+			except:
+				ValueError
 
 		return dataFrame
