b0ca0f919eb62846cabcdacae55b7a5453a8a9b1
Gian <gian.mewes@hsu-hh.de>
2021-05-09 16:46:45 +0200

    Fixed.

--- test/FixTimezone_test.py
+++ test/FixTimezone_test.py
@@ -6,12 +6,8 @@ from FilterBuilder import FilterBuilder
 class FixTimezoneTest(unittest.TestCase):
 
     def testCalculation(self):
-        self.assertEqual(fib(0), 0)
+        
 
 if __name__ == '__main__':
 
     unittest.main()
-
-
-expected_res=pd.Series([7,9,11,13,15])
-pd.testing.assert_series_equal((df1['a']+df2['a']),expected_res,check_names=False)
