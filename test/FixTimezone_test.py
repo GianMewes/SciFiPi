edbc7f9ddd63ceffb2b8d84ae247b862fe74dfa9
Gian <gian.mewes@hsu-hh.de>
2021-05-09 17:13:05 +0200

    added __init__.py

--- test/FixTimezone_test.py
+++ test/FixTimezone_test.py
@@ -1,13 +1,23 @@
 import pandas as pd
 import unittest
+import context
 
-from FilterBuilder import FilterBuilder
+from src.FilterBuilder import FilterBuilder
 
 class FixTimezoneTest(unittest.TestCase):
 
+    def __init__(self):
+
+        self.df = pd.read_csv(r"/testfiles/FixTimezone_test/dirty_data.csv")
+
     def testCalculation(self):
+
+        filterBuilder = FilterBuilder(self.df)
+        self.assertEqual(filterBuilder.fixTimezone(), filterBuilder.fixTimezone())
         
 
-    if __name__ == '__main__':
+if __name__ == '__main__':
 
-        unittest.main()
+    unittest.main()
+
+    
