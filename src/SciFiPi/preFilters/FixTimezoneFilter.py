d297c010f8ceb63008a6447cb483bb8399e3708a
GianMewes <46485683+GianMewes@users.noreply.github.com>
2021-05-10 20:18:03 +0200

    Update FixTimezoneFilter.py

--- src/SciFiPi/preFilters/FixTimezoneFilter.py
+++ src/SciFiPi/preFilters/FixTimezoneFilter.py
@@ -11,7 +11,7 @@ class FixTimezoneFilter(Filter):
         # Check and set timezone information
         # print(dataFrame.head())
         if str(dataFrame.index.tzinfo) == "None":
-            print("[FixTimezoneFilter]: No timezone information found. Setting local timezone: " + get_localzone().zone)
+            print("[FixTimezone-Filter]: No timezone information found. Setting local timezone: " + get_localzone().zone)
             dataFrame = dataFrame.tz_localize(tz = get_localzone())
         else:
             dataFrame = dataFrame.tz_convert(tz = get_localzone())       
