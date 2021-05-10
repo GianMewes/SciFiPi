7d7247c43a4cdd3824c534b6ccc20bd83485ac2c
Aljosha Koecher <aljosha.koecher@hsu-hh.de>
2021-05-10 22:38:31 +0200

    Finished setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
-from distutils.core import setup
+import setuptools
 
 with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
-setup(
+setuptools.setup(
   name = 'SciFiPi',
-  packages = ['SciFiPi'],
   version = '0.0.1', 				
   license='MIT',
   description = 'SciFiPi - The scientific filtering pipeline to clean your machine learning datasets',
@@ -13,21 +12,34 @@ setup(
   long_description_content_type="text/markdown",
   author = 'HSU Institute of Automation',
   #author_email = 'your.email@domain.com',
-  url = 'https://github.com/GianMewes/SciFiPi',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/user/reponame/archive/v_01.tar.gz',    # I explain this later on
-  keywords = ['SOME', 'MEANINGFULL', 'KEYWORDS'],   # Keywords that define your package best
-  install_requires=[            # I get to this in a second
-          'validators',
-          'beautifulsoup4',
-      ],
+  url = 'https://github.com/GianMewes/KEEN',
+  download_url = 'https://github.com/GianMewes/KEEN/archive/refs/tags/v0.0.1-beta.tar.gz',
+  keywords = ['Data Preprocessing', 'Data Filtering', 'Machine Learning', 'AI', 'Data Science', 'python'],
+  install_requires=[
+    'argparse',
+	'ipykernel',
+	'ipython',
+	'ipython-genutils',
+	'numpy',
+	'pandas',
+	'scikit-learn',
+	'fastdtw',
+	'dtwalign',
+	'tzlocal',
+	],
   classifiers=[
-    'Development Status :: 3 - Alpha',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
+    'Development Status :: 3 - Alpha',      # "3 - Alpha", "4 - Beta" or "5 - Production/Stable" -> current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
     'Topic :: Software Development :: Build Tools',
-    'License :: OSI Approved :: MIT License',   # Again, pick a license
-    'Programming Language :: Python :: 3',      #Specify which pyhton versions that you want to support
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
     'Programming Language :: Python :: 3.4',
     'Programming Language :: Python :: 3.5',
     'Programming Language :: Python :: 3.6',
+	'Programming Language :: Python :: 3.7',
+	'Programming Language :: Python :: 3.8',
+	'Programming Language :: Python :: 3.9',
   ],
+  package_dir={"": "src"},
+  packages=setuptools.find_packages(where="src"),
 )
